9516abe6aaadf5a257c559a10af7691c7f9b922a
Clément <clement.caloin@gmail.com>
2020-03-28 22:12:55 +0100

    New views
    Adding new views to the project

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, this is the poll index")
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results for question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id}')

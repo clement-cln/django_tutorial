3dd1a7c8cea83c4629f4181a63a2bfb890d989ab
Clément <clement.caloin@gmail.com>
2020-03-29 22:17:00 +0200

    Added templates
    Added templates for the views and refactored detail url

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Question
 
 # Create your views here.
 def index(request):
     question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
